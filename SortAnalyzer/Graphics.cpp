34575567de94bcaec677c2f14a09991d64e05f5f
AnastasMIPT <AtosBel@ya.ru>
2020-10-19 19:05:13 +0300

    Graphics.cpp and button.h rstructured

--- SortAnalyzer/Graphics.cpp
+++ SortAnalyzer/Graphics.cpp
@@ -1,39 +1,20 @@
 #define GLFW_INCLUDE_GLU
 #include <GLFW/glfw3.h>
-#include "../MyLib/my_vector.h"
-#include "Sorts.h"
-#include "../MyLib/MyInteger.h"
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "../MyLib/my_vector.h"
 #include "Primitives.h"
-
 #include "button.h"
 #include "window.h"
 //#define DEBUG
 #include "../MyLib/debug_info.h"
 
-// https://www.glfw.org/docs/3.0/quick.html
-
-
-
-using MyType = Numeric<int>;
-
-template<>
-size_t Numeric<int>::num_assigns = 0;
-
-template<>
-size_t Numeric<int>::num_comparisons = 0;
 
 AbstractWindowContainer AbstractApplication::windows;
 std::queue <std::unique_ptr<AbstractEvent>> AbstractApplication::event_queue;
 AbstractWindowContainer::WindowList AbstractWindowContainer::subwindows;
 
-
-Vector<MyType> rand_vector (size_t size);
-
-template <typename T>
-Vector<Vector<size_t>> CompGraph_of_sort (SortFunc_t<T> func_sort, size_t start_size = 10, size_t delta = 10, size_t max_size = 100);
 
 void GraphicsMainLoop (SortAnalyzer& app);
 
@@ -61,39 +42,3 @@ void GraphicsMainLoop (SortAnalyzer& app) {
 
 }
 
-
-
-template <typename T>
-Vector<Vector<size_t>> CompGraph_of_sort (SortFunc_t<T> func_sort, size_t start_size, size_t delta, size_t max_size) {
-    size_t size = start_size;
-    Vector<Vector<size_t>> result;
-    while (size <= max_size)
-    {
-        Vector<MyType> arr (rand_vector (size));
-
-        func_sort (&arr[0], 0, size);
-
-        Vector<size_t> temp (3);
-        temp[0] = size;
-        temp[1] = MyType::num_of_comp ();
-        temp[2] = MyType::num_of_assigns ();
-        result.push_back (temp);
-        
-        MyType::num_of_comp_to_zero ();
-        MyType::num_of_assigns_to_zero ();
-        
-        size += delta;
-    }
-    
-    return result;
-}
-
-
-
-Vector<MyType> rand_vector (size_t size) {
-    Vector<MyType> vec (size);
-    for (size_t i = 0; i < size; ++i) {
-        vec[i] = rand () % 1024;
-    }
-    return vec;
-}
--- SortAnalyzer/button.h
+++ SortAnalyzer/button.h
@@ -1,12 +1,31 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include "Sorts.h"
+#include "../MyLib/MyInteger.h"
 
 #include "Primitives.h"
 #include "window.h"
 
+
+using MyType = Numeric<int>;
+
+template<>
+size_t Numeric<int>::num_assigns = 0;
+
+template<>
+size_t Numeric<int>::num_comparisons = 0;
+
+
 template <typename T>
 using SortFunc_t = void (*) (T*, size_t, size_t);
+
+
+
+Vector<MyType> rand_vector (size_t size);
+
+template <typename T>
+Vector<Vector<size_t>> CompGraph_of_sort (SortFunc_t<T> func_sort, size_t start_size = 10, size_t delta = 10, size_t max_size = 100);
 
 
 template <typename T>
@@ -91,5 +110,48 @@ void Button<Functor_t>::onMouseClick (MouseClickEvent* event) {
     }
 }
 
+
+
+
+
+
+
+
+
+
+template <typename T>
+Vector<Vector<size_t>> CompGraph_of_sort (SortFunc_t<T> func_sort, size_t start_size, size_t delta, size_t max_size) {
+    size_t size = start_size;
+    Vector<Vector<size_t>> result;
+    while (size <= max_size)
+    {
+        Vector<MyType> arr (rand_vector (size));
+
+        func_sort (&arr[0], 0, size);
+
+        Vector<size_t> temp (3);
+        temp[0] = size;
+        temp[1] = MyType::num_of_comp ();
+        temp[2] = MyType::num_of_assigns ();
+        result.push_back (temp);
+        
+        MyType::num_of_comp_to_zero ();
+        MyType::num_of_assigns_to_zero ();
+        
+        size += delta;
+    }
+    
+    return result;
+}
+
+
+
+Vector<MyType> rand_vector (size_t size) {
+    Vector<MyType> vec (size);
+    for (size_t i = 0; i < size; ++i) {
+        vec[i] = rand () % 1024;
+    }
+    return vec;
+}
 
 #endif // BUTTON_H
