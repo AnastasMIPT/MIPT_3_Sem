fc7069a1426658289c6747f250025072d0ce9950
AnastasMIPT <AtosBel@ya.ru>
2020-12-11 16:20:12 +0300

    more nice

--- SortAnalyzer/src/Graphics.cpp
+++ SortAnalyzer/src/Graphics.cpp
@@ -14,29 +14,18 @@
 template <>
 GSystem GEngine::system;
 
-WindowContainer AbstractApplication::windows ({0.0, 0.0, 1.0, 1.0, COLORS::DEFAULT_BACKGROUND});
-
-
-
 void GraphicsMainLoop ();
 
 int main  (void)
 {
-    std::unique_ptr<AbstractScrollableWindow> scroll_w (
-        new AbstractScrollableWindow (50, 30)
-    );    
-        
-    std::unique_ptr<ScrollBar> s_bar (
-        new ScrollBar (scroll_w.get (), {0.975, 0.0, 0.02, 1.0, ab::COLORS::LIGHT_GRAY})
-    );
+ 
+    AbstractScrollableWindow scroll_w (50, 30);
+    ScrollBar s_bar (&scroll_w, {0.975, 0.0, 0.02, 1.0, ab::COLORS::LIGHT_GRAY});
 
-    // std::unique_ptr<LabelWindow> label (
-    //     new LabelWindow ("\tanastas", {0.1, 0.1, 0.4, 0.3, ab::COLORS::BLUE})
-    // );
+    Application::addObject (&s_bar);
+ 
 
-    Application::addObject (s_bar.get ());
-    //Application::addObject (label.get ());
-
+ 
     GraphicsMainLoop ();
 
     return 0;
