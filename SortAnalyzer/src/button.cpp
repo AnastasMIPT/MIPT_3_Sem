a9872cd2c8e9745c26ec0d3ca093f8c092e56b6d
AnastasMIPT <AtosBel@ya.ru>
2020-12-11 21:19:31 +0300

    buttom.cpp

---
+++ SortAnalyzer/src/button.cpp
@@ -0,0 +1,23 @@
+#include "button.h"
+
+template<>
+size_t Numeric<int>::num_assigns = 0;
+
+template<>
+size_t Numeric<int>::num_comparisons = 0;
+
+void ScrollFunctor::operator() (Button<ScrollFunctor>* _but, const MouseClickEvent& event) {
+    if (scroll_up) 
+        scroll_window->scrollOnceUp ();
+    else
+        scroll_window->scrollOnceDown ();
+}
+
+
+Vector<MyType> rand_vector (size_t size) {
+    Vector<MyType> vec (size);
+    for (size_t i = 0; i < size; ++i) {
+        vec[i] = rand () % 1024;
+    }
+    return vec;
+}
